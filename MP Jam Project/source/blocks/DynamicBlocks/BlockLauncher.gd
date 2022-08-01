998299316ff70b8761d6cc62783c2c14aae1ec96
mAX <mharris382@gmail.com>
2022-08-01 12:37:37 -0400

    deleted code from blockLauncher

--- MP Jam Project/source/blocks/DynamicBlocks/BlockLauncher.gd
+++ MP Jam Project/source/blocks/DynamicBlocks/BlockLauncher.gd
@@ -1,5 +1,5 @@
 class_name BlockLauncher
-extends Node
+extends Node2D
 
 #magnitude of force to launch blocks
 export var launch_force = 100
@@ -11,39 +11,6 @@ var held_block : DynamicBlock setget held_block_set, held_block_get
 
 func get_launch_force():
 	return launch_direction.transform.xform(Vector2.RIGHT) * launch_force
-
-func drop_held_block():
-	if held_block != null:
-		held_block.mode = RigidBody2D.MODE_RIGID
-		held_block.notify_dropped() #not sure if we'll need this 
-		#see YNGNI principle
-		
-func grab_held_block():
-	if held_block == null:
-		return
-	held_block.mode = RigidBody2D.MODE_KINEMATIC
-	launch_direction.add_child(held_block)
-	
-func throw_held_block():
-	if held_block == null:
-		return
-	var released_block = held_block
-	drop_held_block()
-	#NOTE: we can disable this check if we don't like it
-	if launch_direction.is_colliding():
-		return
-	released_block.add_central_force(launch_force)
-	released_block.notify_thrown(launch_force)
-	
-# Property getter/setters
-func held_block_set(new_block):
-	drop_held_block()
-	held_block = new_block
-	if held_block != null:
-		grab_held_block()
-	
-func held_block_get():
-	return held_block
 
 
 	
