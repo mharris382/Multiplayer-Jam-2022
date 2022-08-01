3b77230b8873167f06cffe39e92aa5bf0e1f4a96
CurioShade <dawid.rp035@gmail.com>
2022-08-01 16:43:58 +0200

    Started implementing throwing
    Currently testing code sits in DynamicBlock. Use newly created DrawTest.tscn in blocks/testing/draw_test to see how it works.

--- MP Jam Project/source/blocks/DynamicBlocks/DynamicBlock.gd
+++ MP Jam Project/source/blocks/DynamicBlocks/DynamicBlock.gd
@@ -1,17 +1,45 @@
 class_name DynamicBlock
 extends RigidBody2D
 
+var impulse_vel = Vector2(100,-100)
+var prev_impulse = impulse_vel
+var gravity = ProjectSettings.get("physics/2d/default_gravity")
+var array : PoolVector2Array = []
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_LEFT and event.pressed:
+			impulse_vel.x -= 5
+			print(impulse_vel.x)
+		if event.scancode == KEY_RIGHT and event.pressed:
+			impulse_vel.x += 5
+			print(impulse_vel.x)
+		if event.scancode == KEY_DOWN and event.pressed:
+			impulse_vel.y += 5
+			print(impulse_vel.y)
+		if event.scancode == KEY_UP and event.pressed:
+			impulse_vel.y -= 5
+			print(impulse_vel.y)
+		if event.scancode == KEY_SPACE and event.pressed:
+			apply_impulse(Vector2(), impulse_vel)
+	
+func get_trajectory_point(step, start_pos, velocity: Vector2, gravity: Vector2, time = 1/60.0) -> Vector2:
+	var t = time
+	var velocity_t = t * velocity
+	var gravity_t = t * t * gravity
+	return start_pos + step * velocity_t + 0.5 * (step * step + step) * gravity_t
 
+func _process(delta):
+	if impulse_vel != prev_impulse:
+		prev_impulse = impulse_vel
+		add_drawing_points(delta)
+		update()
+	
+func add_drawing_points(delta: float):
+	array.resize(0)
+	for i in range(300):
+		array.append(get_trajectory_point(i, Vector2(0,0), impulse_vel, Vector2(0, gravity*gravity_scale), delta))	
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _draw():
+	for result in array:
+		draw_circle(result, 2, Color(255,255,255))
