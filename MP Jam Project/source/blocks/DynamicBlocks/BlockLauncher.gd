4cc98a89466821847a0e4033e6ed4e743edb3461
mAX <mharris382@gmail.com>
2022-08-01 12:38:54 -0400

    fixed typo

--- MP Jam Project/source/blocks/DynamicBlocks/BlockLauncher.gd
+++ MP Jam Project/source/blocks/DynamicBlocks/BlockLauncher.gd
@@ -7,7 +7,6 @@ export var launch_force = 100
 onready var launch_direction :RayCast2D = $"Launch Direction"
 
 var facing_right : bool = true
-var held_block : DynamicBlock setget held_block_set, held_block_get
 
 func get_launch_force():
 	return launch_direction.transform.xform(Vector2.RIGHT) * launch_force
