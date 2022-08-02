1070a3a49a8b5cf649603f06321e20c8b8c5f93f
CurioShade <dawid.rp035@gmail.com>
2022-08-02 15:04:07 +0200

    My fight against Rigidbody2D
    Started making code for teleporting but for some reason it doesn't work.

--- MP Jam Project/source/blocks/DynamicBlocks/DynamicBlock.gd
+++ MP Jam Project/source/blocks/DynamicBlocks/DynamicBlock.gd
@@ -1,6 +1,15 @@
 class_name DynamicBlock
 extends RigidBody2D
 
+var should_teleport: bool = false
+var node_ref = null
+
+func _integrate_forces(state):
+	if should_teleport:
+		var current_pos = state.transform
+		current_pos = node_ref.transform
+		state.transform = current_pos
+		should_teleport = false
 
 func get_trajectory_point(step, start_pos, velocity: Vector2, gravity: Vector2, time = 1/60.0) -> Vector2:
 	var t = time
--- MP Jam Project/source/players/transporter/Transporter.gd
+++ MP Jam Project/source/players/transporter/Transporter.gd
@@ -1,8 +1,11 @@
 extends CharacterBase
 export var throw_force : float = 100
+export (PackedScene) var test_block
+
+var can_throw = false
 
 onready var throw_origin : Node2D #these can be the same transform
-onready var aim_transform : Node2D
+onready var aim_transform : Position2D = $ControlPoints/Aim
 
 
 
@@ -20,20 +23,29 @@ func front_point_position():
 	return front_aim_point.position
 
 func pick_up_a_block():
-	if Input.is_action_just_pressed("ability_%s" % player_id):
+	if Input.is_action_just_pressed("interact_%s" % player_id):
 		var block = move_and_collide(front_point_position(), false, true, true)
 		if block != null:
 			if block.collider.is_in_group("dynamic_block"):
 				var new_block = load("res://source/blocks/DynamicBlocks/PickedBlock.tscn").instance()
 				block.collider.queue_free()
 				front_aim_point.add_child(new_block)
+				can_throw = true
 			
 func _input(event):
 	pick_up_a_block()
+	throw_a_block()
 
 func throw_a_block():
-	var spawn_position = throw_origin.position
-	var impulse_force = aim_transform.transform.xform(Vector2.RIGHT) * throw_force
+	if can_throw and Input.is_action_just_pressed("ability_%s" % player_id):
+		#var spawn_position = throw_origin.position
+		var impulse_force = aim_transform.transform.xform(Vector2.RIGHT) * throw_force
+		var new_block = test_block.instance()
+		get_parent().add_child(new_block)
+		new_block.set("should_teleport", true)
+		new_block.set("node_ref", front_aim_point)
+		front_aim_point.get_child(0).queue_free()
+		can_throw = false
 	#get block PackedScene
 	#instance block
 	#apply force
