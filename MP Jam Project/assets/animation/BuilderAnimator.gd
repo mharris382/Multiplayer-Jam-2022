f9c41c1a91a9a70e0253c79a125c30ce5881ec1e
mAX <mharris382@gmail.com>
2022-08-05 16:55:29 -0400

    Started builder animator

--- MP Jam Project/assets/animation/BuilderAnimator.gd
+++ MP Jam Project/assets/animation/BuilderAnimator.gd
@@ -1,5 +1,15 @@
 extends AnimatedSprite
 
+onready var _animated_sprite = $AnimatedSprite
 
 func _ready():
 	pass
+
+func _process(delta):
+	pass
+
+func jump_up():
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("jump")
+	else:
+		_animated_sprite.play("idle")
