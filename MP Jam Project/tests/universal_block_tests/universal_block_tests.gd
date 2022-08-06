867eb1fa3e89a5567d5c673cd023cf3df0ee35e2
Max <mharris382@gmail.com>
2022-08-06 19:15:25 -0400

    finished spawn dynamic block

--- MP Jam Project/tests/universal_block_tests/universal_block_tests.gd
+++ MP Jam Project/tests/universal_block_tests/universal_block_tests.gd
@@ -2,17 +2,28 @@ extends Node
 
 
 onready var spawns = $"Spawn Points"
-
+var my_coroutine
+var done = false
+var rng
+var last
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var remap = "Furnace_1_On"
-	print(Blocks.remap_block_name(remap))
-	assert(Blocks.remap_block_name(remap) == "Furnace_1")
-	remap = "Furnace_1_Off"
-	print(Blocks.remap_block_name(remap))
+	rng = RandomNumberGenerator.new()
+	
+
+func spawn():
 	var children = spawns.get_children()
-	for child in children:
-		spawn_at_point(child)
+	var cnt = spawns.get_child_count()
+	var index = rng.randi_range(0, cnt-1)
+	if index == last:
+		if index == 0:
+			index = cnt-1
+		elif index == cnt-1:
+			index = 0
+		else:
+			index+=1
+	spawn_at_point(spawns.get_child(index))
+	done = true
 
 func spawn_at_point(node : Node2D):
 	var block_name = "Block_%s" % node.name
@@ -22,4 +33,11 @@ func spawn_at_point(node : Node2D):
 	if not (Blocks.get_block_data(block_name) != null):
 		print("Blocks could not find block named ", block_name)
 		return
-	Blocks.instance_dynamic_block_at_location(block_name, node.position, self)
+	print("Spawned ", block_name)
+	var block =  Blocks.instance_dynamic_block_at_location(block_name, node.position, self)
+	if node.get_child_count() > 0:
+		block.add_child(node.get_child(0))
+
+
+func _on_Button_button_down():
+	spawn()
