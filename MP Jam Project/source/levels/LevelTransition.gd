995ffb0603b80cd07966d5c97d7c9c57d78f23fd
CurioShade <dawid.rp035@gmail.com>
2022-08-07 18:20:46 +0200

    Published version

--- MP Jam Project/source/levels/LevelTransition.gd
+++ MP Jam Project/source/levels/LevelTransition.gd
@@ -4,10 +4,6 @@ signal move_to_puzzle(puzzle)
 
 export var puzzle: Resource
 
-func _ready():
-	if puzzle.completed:
-		modulate = Color(49, 164, 51, 255)
-
 func _on_LevelTransition_body_entered(body):
 	if body is PlayerMovement:
 		if puzzle.completed == false:
