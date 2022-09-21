bf5ac1dede311f6064a43df47c0d43f8966b01cc
mAX <mharris382@gmail.com>
2022-09-21 12:31:16 -0400

    Camera fix

--- Refactored Project/source/game/CameraController.gd
+++ Refactored Project/source/game/CameraController.gd
@@ -1,3 +1,4 @@
+tool
 extends Node
 #NOTE: this script was originally meant to allow more than 2 avatars but I simplified it, as it is currently it will only function with 0 avatars or 2 avatars
 var avatars = [null, null]
@@ -6,6 +7,10 @@ var target_zoom = Vector2(1,1)
 export var avatar_padding = Vector2(25, 25)
 export var prefer_zoom= Vector2(1,1)
 export var limit_zoom = Vector2(4, 4)
+export var reset_speed = 4.0
+
+export var lower_offset = Vector2(0, -214)
+export var upper_offset = Vector2(0, -388)
 
 onready var camera = $Camera2D
 onready var builder_transform = $"Builder"
@@ -18,36 +23,73 @@ onready var transporter_transform = $"Transporter"
 func _ready():
 	camera.current = true
 	target_zoom = camera.zoom
-	avatars[0] = builder_transform
-	avatars[1] = transporter_transform
+	_get_avatars()
 	
 
 func _process(delta):
+	if camera == null:
+		camera = get_node("Camera2D")
+		camera.current = true
 	var current_camera_position = camera.position
 	camera.current = true
 	
+	_set_offsets()
 	var target_location = _get_target_location()
 	var target_zoom = _get_target_zoom()
-	
 	camera.position = target_location
 	camera.zoom = target_zoom
+	camera.zoom = Vector2(lerp(target_zoom.x, prefer_zoom.x, delta), lerp(target_zoom.y, prefer_zoom.y, delta * reset_speed))
+	
 
 #-----------------------------------------------
 #helpers
 
 func _get_avatar_position(avatar_index :int):
+	_get_avatars()
+	
 	if avatar_index > avatars.size():
-		return camera.position
+		printerr("index is invalid: ", avatar_index)
+		avatar_index = 0
+		
 	var avatar = avatars[avatar_index] as Node2D
 	if avatar == null:
-		return camera.position
-	return avatar.position
-
+		printerr("Avatar at index is null", avatar_index)
+		return camera.global_position
+	return avatar.global_position
+	
+func _get_avatar_actual_position(avatar_index :int):
+	_get_avatars()
+	
+	if avatar_index > avatars.size():
+		printerr("index is invalid: ", avatar_index)
+		avatar_index = 0
+		
+	var avatar = avatars[avatar_index] as Node2D
+	if avatar == null:
+		printerr("Avatar at index is null", avatar_index)
+		return camera.global_position
+	return avatar.get_parent().global_position
+	
 func _get_target_location():
 	var target_position = _calculate_target_position()
 	if target_position == null: #edge case: null if avatar count = 0
-		return camera.position
+		return camera.global_position
 	return target_position
+	
+func _set_offsets():
+	var p1 = _get_avatar_actual_position(0) as Vector2
+	var p2 = _get_avatar_actual_position(1) as Vector2
+	if p1.y > p2.y:
+		var lower = avatars[0] as Node2D
+		var upper = avatars[1] as Node2D
+		lower.position = lower_offset
+		upper.position = upper_offset
+	else:
+		var lower = avatars[1] as Node2D
+		var upper = avatars[0] as Node2D
+		lower.position = lower_offset
+		upper.position = upper_offset
+	
 
 func _get_required_size():#to ensure all player avatars are visible
 	var p1 = _get_avatar_position(0) as Vector2
@@ -56,14 +98,19 @@ func _get_required_size():#to ensure all player avatars are visible
 	var avatar_min_pos = Vector2(min(p1.x, p2.x),  min(p1.y, p2.y))
 	var avatar_max_pos = Vector2(max(p1.x, p2.x),  max(p1.y, p2.y))
 	var dif = avatar_max_pos - avatar_min_pos
+	if dif.x > dif.y:
+		dif = Vector2(dif.x, dif.x)
+	else:
+		dif = Vector2(dif.y, dif.y)
 	dif += avatar_padding
 	return dif
 
+var size2 = Vector2(1080, 1080)
 func _get_target_zoom():
 	assert(limit_zoom.x >= prefer_zoom.x)
 	assert(limit_zoom.y >= prefer_zoom.y)
 	
-	var viewport_size = get_viewport().size
+	var viewport_size = size2# get_viewport().size
 	
 	var p1 = _get_avatar_position(0) as Vector2
 	var p2 = _get_avatar_position(1) as Vector2
@@ -73,6 +120,8 @@ func _get_target_zoom():
 	
 	
 	var required_size = _get_required_size() #required size to ensure all player avatars are visible
+	if required_size.x < target_zoom.x or required_size.y < target_zoom.y:
+		print("required size = ", required_size, " target zoom = ", target_zoom)
 	var current_size  = viewport_size * camera.zoom #current size of viewport in worldspace (after considering zoom)
 	var prefer_size = viewport_size * prefer_zoom #prefered size is the most zoomed in allowed
 	var limit_size = viewport_size *  limit_zoom #limit size is the most zoomed out allowed
@@ -83,13 +132,20 @@ func _get_target_zoom():
 	target_size.y = clamp(target_size.y, prefer_size.y, limit_size.y )
 	
 	var target_zoom = target_size/viewport_size
-	var zoom_max = max(target_zoom.x, target_zoom.y)
+	var zoom_max = min(target_zoom.x, target_zoom.y)
 	return Vector2(zoom_max, zoom_max)
-
+func _get_avatars():
+	if avatars.size() != 2 or (avatars[0] == null or avatars[1] == null):
+		avatars.clear()
+		avatars.append(get_parent().get_node("Builder/Camera Target"))
+		avatars.append(get_parent().get_node("Transporter/Camera Target"))
+		
 func _calculate_target_position():
 	var target_position = Vector2.ZERO #initialize to zero
 	var count = 0
 	
+	_get_avatars()
+		
 	for avatar in avatars:
 		assert(avatar != null)
 		if avatar != null:
@@ -98,6 +154,7 @@ func _calculate_target_position():
 	
 	if count == 0:
 		camera.position = Vector2.ZERO
+		printerr("No avatars found!")
 		return null
 	
 	#this is a simple average, can be turned into a weighted average with some modifications
